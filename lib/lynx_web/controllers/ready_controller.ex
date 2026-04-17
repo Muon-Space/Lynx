7d8e8fe0c36648b44f915ca04aab2467c18c0719
Aron Gates <aron@muonspace.com>
2026-04-17 11:40:58 +0100

    fix: readiness will report ready even when not installed

--- lib/lynx_web/controllers/ready_controller.ex
+++ lib/lynx_web/controllers/ready_controller.ex
@@ -3,32 +3,19 @@
 # license that can be found in the LICENSE file.
 
 defmodule LynxWeb.ReadyController do
-  @moduledoc """
-  Ready Controller
-  """
-
   use LynxWeb, :controller
 
-  require Logger
-
-  alias Lynx.Module.SettingsModule
-
-  @doc """
-  Ready Endpoint
-  """
   def ready(conn, _params) do
-    Logger.info("Application is ready")
+    case Ecto.Adapters.SQL.query(Lynx.Repo, "SELECT 1") do
+      {:ok, _} ->
+        conn
+        |> put_resp_content_type("application/json")
+        |> send_resp(:ok, Jason.encode!(%{status: "ok"}))
 
-    app_name = SettingsModule.get_config("app_name", "")
-
-    if app_name == "" do
-      conn
-      |> put_resp_content_type("application/json")
-      |> send_resp(:internal_server_error, Jason.encode!(%{status: "not ok"}))
-    else
-      conn
-      |> put_resp_content_type("application/json")
-      |> send_resp(:ok, Jason.encode!(%{status: "ok"}))
+      {:error, _} ->
+        conn
+        |> put_resp_content_type("application/json")
+        |> send_resp(:service_unavailable, Jason.encode!(%{status: "database unavailable"}))
     end
   end
 end
